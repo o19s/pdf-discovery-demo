ba9c5e4743bd4b9fbf3ae1c8d8f33123b499819a
epugh <epugh@opensourceconnections.com>
2019-08-17 09:06:41 -0400

    update notes

--- ocr/extract-old-style.ps1
+++ ocr/extract-old-style.ps1
@@ -9,6 +9,7 @@ Write-Host "About to hocr PDF file " + $pdf_file
 #$result = curl -T $pdf_file http://localhost:9998/rmeta --header "X-Tika-OCRLanguage: eng" --header "X-Tika-PDFOcrStrategy: ocr_only" --header "X-Tika-OCRoutputType: hocr"
 $result = java -cp ./tika-properties:tika-app-1.22.jar org.apache.tika.cli.TikaCLI --xmp --jsonRecursive --extract --pretty-print -x $pdf_file
 
+Write-Host "Done with extract, now converting"
 
 $outputFile = Split-Path $pdf_file -leaf
 
@@ -16,18 +17,13 @@ $extract_file = "./extracts/" + $outputFile + ".xml"
 
 Set-Content -Path $extract_file -Value $result
 
-Write-Host "Done with extract, now converting"
-#Write-Host $result
-Write-Host "Done with Result"
-
-# $json = (Get-Content "output_brainard20190711a.json" -Raw) | ConvertFrom-Json
 $json = $result | ConvertFrom-Json
 
 
 $hocr_xml = [xml]$json.'X-TIKA:content'
 
 #Write-Host $result
-
+Write-Host "Done with Result"
 $nsmgr = New-Object System.XML.XmlNamespaceManager($hocr_xml.NameTable)
 $nsmgr.AddNamespace('xsi','http://www.w3.org/2001/XMLSchema-instance')
 $nsmgr.AddNamespace('x','http://www.w3.org/1999/xhtml')
@@ -35,10 +31,16 @@ $nsmgr.AddNamespace('x','http://www.w3.org/1999/xhtml')
 $words = $hocr_xml.SelectNodes("//x:span[@class='ocrx_word']",$nsmgr)
 
 $hocr_output = ''
+
+# We keep the full formatting, and then base 64 encode it as the payload.
+# String payload = "bbox 504 543 626 581; x_wconf 93";
 foreach ($word in $words) {
   #Write-Host $word.title
-  $split = $word.title.split(";")
-  $payload = $split[0].Replace("bbox ","")
+  $payload = $word.title
+  # bbox 452 315 488 332; x_wconf 96
+  $payload=[Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($payload))
+  #$split = $word.title.split(";")
+  #$payload = $split[0].Replace("bbox ","")
   #Write-Host $split[0]
   $hocr_output = $hocr_output + $word.InnerText + "|" + $payload + " "
 }
