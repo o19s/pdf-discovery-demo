4892141d3ff1a2b1fa439ccb3d04d3bf70b9da68
Daniel Worley <danworley@gmail.com>
2019-09-17 12:12:05 -0400

    Update search logic to work with page level docs

--- app/search.js
+++ app/search.js
@@ -13,17 +13,17 @@ $(document).ready(function () {
   // reset the search input
   $('form input[type="text"]')[0].value = decodeURIComponent(query.replace(/\+/g, '%20'));
 
-  $.getJSON('//' + window.location.hostname + ':8983/solr/documents/select?q=' + query + '&fl=id,content,path,page_dimensions&hl=on&hl.snippets=10&hl.fl=content&indent=on&wt=json&pl=on&echoParams=all', function(data) {
-    renderResultsList(data.response.docs)
+  $.getJSON('//' + window.location.hostname + ':8983/solr/documents/select?q=' + query + '&fl=id,content,path,page_dimensions&group=on&group.field=doc_id', function(data) {
+    renderResultsList(data.grouped.doc_id.groups)
   })
 
   function renderResultsList(results) {
     $(results).each(function(index, result) {
       var resultMarkup =  '<div class="result-item">';
-          resultMarkup +=  '<a href="/details.html?id=' + result.id + '&query=' + query +'">';
-          resultMarkup +=    '<h2>' + result.id + '</h2>';
+          resultMarkup +=  '<a href="/details.html?id=' + result.groupValue + '&query=' + query +'">';
+          resultMarkup +=    '<h2>' + result.groupValue + '</h2>';
           resultMarkup +=  '</a>';
-          resultMarkup += '<p>' + result.content.slice(0, 200) + '</p>';
+          resultMarkup += '<p>' + result.doclist.docs[0].content.slice(0, 200) + '</p>';
           resultMarkup += '</div>';
 
       $resultsList.append(resultMarkup)
