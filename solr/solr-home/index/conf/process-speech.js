657399ae7435ac41d595946252296d5d126887d0
epugh <epugh@opensourceconnections.com>
2019-10-29 15:32:30 -0400

    first cut of full script

--- solr/solr-home/index/conf/process-speech.js
+++ solr/solr-home/index/conf/process-speech.js
@@ -11,6 +11,8 @@ function processAdd(cmd) {
   id = doc.getFieldValue("id");
   logger.info("process-speech#processAdd: id=" + id);
 
+  logger.info("Here comes some base 64: " + Packages.org.apache.solr.common.util.Base64.byteArrayToBase64(id.getBytes()));
+
   doc.setField( "parent_id", doc.getFieldValue("id"));
   doc.setField( "content_type", "parentDocument");
   doc.setField( "path", doc.getFieldValue("attr_stream_name"));
@@ -19,7 +21,7 @@ function processAdd(cmd) {
 
   var pages = content.split(/ocr_page page_1/);
   // Skip page one, it's garbage
-  for (var page_number = 1; page_number < pages.length;page_number++){
+  for (var page_number = 0; page_number < pages.length -1;page_number++){
     logger.info("process-speech#processAdd: Looking at page " + page_number);
     var childDoc = new Packages.org.apache.solr.common.SolrInputDocument();
     childDoc.setField( "id", doc.getFieldValue("id") + "_" + page_number);
@@ -27,14 +29,69 @@ function processAdd(cmd) {
     childDoc.setField( "content_type", "childDocument");
 
 
-    var lines = pages[page_number].split("\n");
+    // Grab content after the split on ocr_page to get the page_dimension
+    var lines = pages[page_number+1].split("\n");
     var page_dimension = lines[0].split(";")[1].replace("bbox","").trim()
-    childDoc.setField("page_dimension", page_dimension);
-    childDoc.setField("page_number", page_number);
 
-    childDoc.setField("content", "content_coming soon");
-    childDoc.setField("content_ocr", "content_ocr coming soon");
-    childDoc.setField("content_extracted", "content_extracted coming soon");
+    // Grab content before the split on ocr_page to get text_extracted
+    var lines = pages[page_number].split("\n");
+    var foundPage = -1;
+    var foundOcr = -1;
+    var extracted_text = "";
+    for (i = 0; i < lines.length;i++){
+
+      if (lines[i].trim() == 'page'){
+        foundPage = i;
+      }
+      if (lines[i].trim() == 'ocr'){
+        foundOcr = i;
+      }
+      if (foundPage > -1 & foundOcr > -1){
+
+        extracted_text = lines.slice(foundPage+1,foundOcr).join(" ");
+//        logger.info("Slice from " + foundPage + " to " + foundOcr);
+        foundPage = -1;
+        foundOcr = -1;
+      }
+    }
+
+
+
+    var page_words = pages[page_number+1].split(/ocrx_word/);
+
+    var tokens_array = []
+    var tokens_payloads_array = []
+    for (var i = 1; i <= page_words.length;i++){
+      var line = page_words[i];
+      if (line == undefined){
+        continue;
+      }
+      var tokens = line.trim().split(" ");
+      if (tokens[9] === 'ltr'){
+        token = tokens[10];
+      }
+      else if (tokens[8] === 'eng'){
+        token = tokens[9];
+      }
+      else {
+        logger.info("process-speech#processAdd: Unknown line formatting: " + tokens);
+      }
+      token = token.replace(/\s+/g, '');
+      tokens_array.push(token);
+
+      payload = line.split(/bbox/)[1].split(/;/)[0].trim();
+      payload = Packages.org.apache.solr.common.util.Base64.byteArrayToBase64(payload.getBytes());
+      tokens_payloads_array.push(token + '|' + payload)
+
+    }
+
+
+    childDoc.setField("page_dimension", page_dimension);
+    childDoc.setField("page_number", page_number+1);
+
+    childDoc.setField("content", tokens_array.join(" "));
+    childDoc.setField("content_ocr", tokens_payloads_array.join(" "));
+    childDoc.setField("content_extracted", extracted_text);
 
     doc.addChildDocument(childDoc);
   }
