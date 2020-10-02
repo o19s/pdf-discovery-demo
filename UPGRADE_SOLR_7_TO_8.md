5515c3571498545497b09b2c043206d144380d28
epugh <epugh@opensourceconnections.com>
2020-10-02 17:29:41 -0400

    upgrade migration in place

---
+++ UPGRADE_SOLR_7_TO_8.md
@@ -0,0 +1,46 @@
+Start with
+https://opensourceconnections.com/?p=5595&preview=true
+
+
+
+
+1. Lets Backup.
+
+> curl 'http://localhost:8983/solr/admin/collections?action=BACKUP&name=documentsv7&collection=documents&location=/solr_backup' -H 'Content-type:application/json'
+
+Then everything ends up `/solr_backup`.
+
+1. Upgrade to Solr 8.6.2
+
+
+
+Update `docker-compose.yml` for Solr to 8.6.2
+
+
+1. Deploy `security.json`
+
+We need a security.json file to be deployed in order to be able to use API's to deploy configsets.
+
+> /opt/solr/server/scripts/cloud-scripts/zkcli.sh -zkhost zookeeper:2181 -cmd putfile /security.json /code/security.json
+
+1. Lets Restore
+
+> curl 'http://localhost:8983/solr/admin/collections?action=RESTORE&name=documentsv7&location=/solr_backup&collection=restored_documents' -H 'Content-type:application/json'
+
+
+1. Lets deploy the Updated configset.
+
+cd ./solr/solr_home/ecommerce/conf
+
+edit `solrconfig.xml` and set luceneMatchVersion to 8.6
+
+> (cd solr/solr-home/index/conf && zip -r - *) > ./solr/solr-home/documentsv8.zip
+
+> curl  --user solr:SolrRocks -X POST --header "Content-Type:application/octet-stream" --data-binary @./solr/solr-home/documentsv8.zip "http://localhost:8983/solr/admin/configs?action=UPLOAD&name=documentsv8"
+
+1. Create a new Collection using the Solr Admin called `documentsv8` using the new configset.
+
+> curl 'http://localhost:8983/solr/admin/collections?action=REINDEXCOLLECTION&name=documentsv7&target=documentsv8&configName=documentsv8&numShards=2&maxShardsPerNode=2&async=99' -H 'Content-type:application/json'
+
+
+1. Point the UI at the new front end and confirm it works.
--- ocr/init/init.sh
+++ ocr/init/init.sh
@@ -9,9 +9,14 @@
 #sleep 15
 ./wait-for-solr.sh --max-attempts 10 --wait-seconds 4 --solr-url http://solr:8983
 
-#echo "Uploading security.json to ZK"
+echo "Uploading security.json to ZK"
+
+java -jar ./jackhanna-0.0.4-SNAPSHOT.jar zookeeper:2181 putfile --file security.json --zkFile /security.json
+
+
+
 #/opt/solr/server/scripts/cloud-scripts/zkcli.sh -zkhost zookeeper:2181 -cmd putfile /security.json /code/security.json
-#sleep 5 #give ZK chance to sync with Solr first
+sleep 5 #give ZK chance to sync with Solr first
 
 #echo "Creating admin user"
 #curl --user solr:SolrRocks http://solr:8983/solr/admin/authentication -H 'Content-type:application/json' -d '{
@@ -29,7 +34,7 @@
 #                "delete-user": ["solr"]}'
 
 echo "Creating documents collection...."
-curl --user admin:3YnRnaMk7sLbc "http://solr:8983/solr/admin/collections?action=CREATE&name=documents&collection.configName=configuration1&numShards=2&maxShardsPerNode=2" #place holder
+curl --user solr:SolrRocks "http://solr:8983/solr/admin/collections?action=CREATE&name=documents&collection.configName=configuration1&numShards=2&maxShardsPerNode=2" #place holder
 
 echo "Sleeping 5"
 sleep 5
@@ -39,11 +44,3 @@ echo "Load all the extracts"
 ./load_sample_files.sh /docs_for_solr http://solr:8983/solr/documents/update
 ./load_sample_files.sh /docs_for_solr2 http://solr:8983/solr/documents/update
 echo "Done with setup"
-
-# No longer need this because the PDF's are ALSO served up in the ./app image.
-# https://github.com/http-party/http-server
-#http-server ../files --cors --ssl --cert ./cert.pem --key ./key.pem -p 8443
-#http-server ../files --cors -p 8443
-
-#cd /files
-#python3 -m http.server 8080
